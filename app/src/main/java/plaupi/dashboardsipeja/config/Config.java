b9c04a45441471b24faf6a20aa7b5a509bed5eb8
azizashari <aziz.ashari1@gmail.com>
2017-03-09 22:46:01 +0700

    First

---
+++ app/src/main/java/plaupi/dashboardsipeja/config/Config.java
@@ -0,0 +1,17 @@
+package plaupi.dashboardsipeja.config;
+
+/**
+ * Created by Aziz on 09/03/2017.
+ */
+
+public class Config {
+
+    public static final String URL = "http://sipeja.pe.hu/";
+
+    //JSON untuk Pendapatan Per Lab
+    public static final String JSON_ARRAY = "result";
+    public static final String KEY_BIAYA = "biaya"; //total pendapatan per lab
+    public static final String KEY_LABS = "nama"; //nama laboratorium
+    public static final String KEY_TAHUN = "tahun"; //tahun
+}
+
